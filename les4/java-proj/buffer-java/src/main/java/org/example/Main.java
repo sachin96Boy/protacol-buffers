e29d16e0249e03264494bf21e261d08a1577f61d
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-08-24 20:31:31 +0530

    chor: simple proto to java and add methods to read write

--- les4/java-proj/buffer-java/src/main/java/org/example/Main.java
+++ les4/java-proj/buffer-java/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import example.simple.Simple.simpleMessage;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        simpleMessage.Builder builder = simpleMessage.newBuilder();
+
+//        setting an id
+//        builder.setId(42);
+//        builder.setIsSimple(true);
+//        builder.setName("My simple message");
+
+
+//        simple fields
+        builder.setId(42).setIsSimple(true).setName("My simple message name section");
+
+//        repated fields
+        builder.addSimpleList(1);
+        builder.addSimpleList(2);
+        builder.addSimpleList(3);
+
+        builder.addAllSimpleList(Arrays.asList(4,5,6));
+
+//        builder.clearSimpleList();
+
+
+        simpleMessage message = builder.build();
+
     }
 }
---
+++ les4/java-proj/buffer-java/src/main/java/org/example/simpletest/protobuf/SimpleMain.java
@@ -0,0 +1,7 @@
+package org.example.simpletest.protobuf;
+
+public class SimpleMain {
+    public static void main(String[] args) {
+        System.out.println("Hello from here");
+    }
+}
